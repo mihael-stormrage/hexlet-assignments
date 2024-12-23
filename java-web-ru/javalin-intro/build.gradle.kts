1eda49e30dc833df345ed4303824b5fe873abb5a
@hexlet/cli <support@hexlet.io>
2024-12-23 14:55:21 +0700

    submit java-web-ru/javalin-intro

---
+++ java-web-ru/javalin-intro/build.gradle.kts
@@ -0,0 +1,57 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.48.0"
+    checkstyle
+    application
+}
+
+application {
+    mainClass.set("exercise.App")
+}
+
+group = "exercise"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // BEGIN
+    implementation("io.javalin:javalin:6.4.0")
+    // END
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.konghq:unirest-java:3.13.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+checkstyle {
+    toolVersion = "8.41"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.register<Checkstyle>("checkExercises") {
+    file(".").listFiles { file -> file.isDirectory }?.forEach { course ->
+        val exercise = (project.findProperty("exercise") ?: "") as String
+        source(course.name + '/' + exercise)
+        include("**/*.java")
+        exclude("**/build/**")
+        classpath = files()
+    }
+}
