24e125affe14c519b81454804f4e71182a7bc85e
@hexlet/cli <support@hexlet.io>
2025-01-14 00:15:12 +0700

    submit java-spring-boot-ru/http-crud

---
+++ java-spring-boot-ru/http-crud/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    checkstyle
+    application
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("com.github.ben-manes.versions") version "0.48.0"
+    id("io.freefair.lombok") version "8.6"
+}
+
+group = "exercise"
+
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("exercise.Application") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("net.datafaker:datafaker:2.0.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+}
+
+checkstyle {
+    toolVersion = "8.41"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
+
+tasks.register<Checkstyle>("checkExercises") {
+    file(".").listFiles { file -> file.isDirectory }?.forEach { course ->
+        val exercise = (project.findProperty("exercise") ?: "") as String
+        source(course.name + '/' + exercise)
+        include("**/*.java")
+        exclude("**/build/**")
+        exclude("**/generated/**")
+        classpath = files()
+    }
+}
